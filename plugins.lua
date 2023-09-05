797dc35627ecbb6a742304d62603b14e3f4dacd3
Majid Zahedi <emajidzahedi@gmail.com>
2023-09-05 11:05:24 +0330

    some plugins

--- plugins.lua
+++ plugins.lua
@@ -2,7 +2,6 @@ local overrides = require "custom.configs.overrides"
 
 ---@type NvPluginSpec[]
 local plugins = {
-
   -- Override plugin definition options
 
   {
@@ -58,14 +57,83 @@ local plugins = {
       }
     end,
   },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = false,
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-ts-autotag").setup {
+        -- your config
+      }
+    end,
+    lazy = true,
+    event = "VeryLazy",
+  },
+  {
+    "folke/zen-mode.nvim",
+    lazy = false,
+    opts = overrides.zenmode,
+  },
+  {
+    "folke/twilight.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  {
+    "folke/todo-comments.nvim",
+    lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    event = "BufReadPost",
+    keys = overrides.fold.keys,
+    init = function()
+      -- INFO fold commands usually change the foldlevel, which fixes folds, e.g.
+      -- auto-closing them after leaving insert mode, however ufo does not seem to
+      -- have equivalents for zr and zm because there is no saved fold level.
+      -- Consequently, the vim-internal fold levels need to be disabled by setting
+      -- them to 99
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+    end,
+    opts = overrides.fold.opts,
+    config = function(_, opts)
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("local_detach_ufo", { clear = true }),
+        pattern = opts.filetype_exclude,
+        callback = function()
+          require("ufo").detach()
+        end,
+      })
 
+      vim.opt.foldlevelstart = 99
+      require("ufo").setup(opts)
+    end,
+  },
   -- All NvChad plugins are lazy-loaded by default
   -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
   -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-  -- {
-  --   "mg979/vim-visual-multi",
-  --   lazy = false,
-  -- }
+  {
+    "mg979/vim-visual-multi",
+    lazy = false,
+  },
 }
 
 return plugins
